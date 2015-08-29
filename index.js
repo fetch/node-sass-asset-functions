1f1ddcddfbd5b605325a9d1aedad93b12bf7b733
Koen Punt <koen@koenpunt.nl>
2015-08-29 13:00:56 +0200

    remove actual cache busting function
    take asset_host into account when generating the url

--- index.js
+++ index.js
@@ -1,46 +1,11 @@
 var fs = require('fs')
   , path = require('path')
   , util = require('util')
-  , crypto = require('crypto');
+  , url = require('url');
 
 var sass = require('node-sass')
   , sizeOf = require('image-size')
   , async = require('async');
-
-function extend(destination, source) {
-  for (var property in source)
-    destination[property] = source[property];
-  return destination;
-}
-
-var file_digest = function(path, algo, cb) {
-  var shasum, stream;
-  shasum = crypto.createHash(algo);
-  stream = fs.ReadStream(path);
-  stream.on('data', function(d) {
-    shasum.update(d);
-  });
-  stream.on('end', function() {
-    cb(shasum.digest('hex'));
-  });
-};
-
-var cache_buster = function(http_path, real_path, cb) {
-  fs.exists(real_path, function(exists) {
-    var basename, dirname, extname;
-    if (exists) {
-      dirname = path.dirname(http_path);
-      extname = path.extname(http_path);
-      basename = path.basename(http_path, extname);
-      file_digest(real_path, 'sha1', function(hash) {
-        var new_path = util.format('%s/%s-%s%s', dirname, basename, hash, extname);
-        cb(new_path);
-      });
-    } else {
-      cb(http_path);
-    }
-  });
-};
 
 var Processor = function(options) {
   this.options = options || {};
@@ -51,6 +16,7 @@ var Processor = function(options) {
     http_images_path: '/images',
     http_fonts_path: '/fonts'
   };
+
   var path;
   for(path in this.paths){
     if(this.options[path]){
@@ -59,28 +25,32 @@ var Processor = function(options) {
   }
 };
 
-Processor.prototype.cache_buster = function(filepath, real_path, done) {
-  if (!this.options.cache_buster) {
-    return done(filepath);
+Processor.prototype.asset_cache_buster = function(http_path, real_path, done) {
+  if (typeof this.options.asset_cache_buster !== 'function') {
+    throw new Error('asset_cache_buster should be a function');
   }
-  if (typeof this.options.cache_buster == 'function') {
-    return this.options.cache_buster(filepath, real_path, done);
-  }
-  return cache_buster(filepath, real_path, done);
+  var http_path_url = url.parse(http_path);
+  this.options.asset_cache_buster(http_path, real_path, function(value) {
+    if (typeof value == 'string') {
+      http_path_url.search = value;
+    } else {
+      http_path_url.path = value.path;
+      http_path_url.search = value.query;
+    }
+    done(url.format(http_path_url));
+  });
 };
 
 Processor.prototype.asset_host = function(filepath, done) {
   if (!this.options.asset_host) {
     return done(filepath);
   }
-  var resolve = function(host){
+  var resolve = function(host) {
     done(url.resolve(host, filepath));
   };
   var asset_host = this.options.asset_host;
   if (typeof asset_host == 'function') {
-    asset_host(filepath, function(host){
-      resolve(host);
-    });
+    asset_host(filepath, resolve);
   } else {
     resolve(asset_host);
   }
@@ -88,7 +58,7 @@ Processor.prototype.asset_host = function(filepath, done) {
 
 Processor.prototype.real_path = function(filepath, segment) {
   var fragmentIndex = filepath.indexOf('#');
-  if(~fragmentIndex){
+  if (~fragmentIndex) {
     filepath = filepath.substring(0, fragmentIndex);
   }
   return path.resolve(path.join(this.paths[segment + '_path'], filepath));
@@ -108,10 +78,14 @@ Processor.prototype.image_height = function(filepath) {
 
 Processor.prototype.asset_url = function(filepath, segment, done) {
   var http_path = this.http_path(filepath, segment);
-  if (this.cache_buster) {
-    this.cache_buster(http_path, this.real_path(filepath, segment), done);
+  var asset_host = function(http_path){
+    this.asset_host(http_path, done);
+  }.bind(this);
+
+  if(this.options.asset_cache_buster) {
+    this.asset_cache_buster(http_path, this.real_path(filepath, segment), asset_host);
   } else {
-    done(http_path);
+    done(asset_host);
   }
 };
 
@@ -123,41 +97,42 @@ Processor.prototype.font_url = function(filepath, done) {
   this.asset_url(filepath, 'fonts', done);
 };
 
+var font_format_map = {
+  eot: 'embedded-opentype',
+  ttf: 'truetype'
+};
+
 Processor.prototype.font_files = function(files, done) {
-  var format_map = {
-    eot: 'embedded-opentype',
-    ttf: 'truetype'
-  };
   var i = 0, parts, ext, dir, file;
   var result = [];
 
   var font_url = this.font_url.bind(this);
 
-  async.map(files, function(file, done){
+  async.map(files, function(file, done) {
     parts = file.split('#');
     filename = parts[0];
     anchor = parts[1];
     ext = path.extname(filename);
     format = ext.substring(1);
 
-    if (format_map[format]) {
-      format = format_map[format];
+    if (font_format_map[format]) {
+      format = font_format_map[format];
     }
-    font_url(file, function(url){
+    font_url(file, function(url) {
       done(null, {url: url, format: format});
     });
-  }, function(err, results){
+  }, function(err, results) {
     done(results);
   });
 };
 
-module.exports = function(options){
+module.exports = function(options) {
   var opts = options || {};
   var processor = new Processor(opts);
 
   return {
     'image-url($filename: null, $only_path: false)': function(filename, only_path, done) {
-      processor.image_url(filename.getValue(), function(url){
+      processor.image_url(filename.getValue(), function(url) {
         if(!only_path.getValue()) url = 'url(\'' + url + '\')';
         done(new sass.types.String(url));
       });
@@ -171,7 +146,7 @@ module.exports = function(options){
       return new sass.types.Number(image_height, 'px');
     },
     'font-url($filename: null, $only-path: false)': function(filename, only_path, done) {
-      processor.font_url(filename.getValue(), function(url){
+      processor.font_url(filename.getValue(), function(url) {
         if(!only_path.getValue()) url = 'url(\'' + url + '\')';
         done(new sass.types.String(url));
       });
@@ -182,7 +157,7 @@ module.exports = function(options){
         filenames[i] = list.getValue(i).getValue();
       }
 
-      processor.font_files(filenames, function(files){
+      processor.font_files(filenames, function(files) {
         list = new sass.types.List(files.length);
         i = 0;
         for (;i < l;++i) {
@@ -192,5 +167,4 @@ module.exports = function(options){
       });
     }
   };
-
 };
